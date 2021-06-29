934bd0a6ae9e33097f81922e8926d0c8d6342099
nguyentggt <85239083+nguyentggt@users.noreply.github.com>
2021-06-29 19:05:40 +0700

    Add files via upload
    Calculating triangle length using Pythagorean theorem and Math class

---
+++ lesson2/triangle.java
@@ -0,0 +1,10 @@
+package triangle;
+
+public class triangle {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        double c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        System.out.println(c);
+    }
+}
