9dbe4517bc053cab0267d13b18458f4d5396fdd1
nguyentggt <85239083+nguyentggt@users.noreply.github.com>
2021-06-30 17:15:09 +0700

    Add files via upload

---
+++ lesson 3/ContrlFlow.java
@@ -0,0 +1,10 @@
+package homework.controlflow;
+
+public class ContrlFlow {
+    String str = "Hello";
+    System.out.println(str.indexOf("e"));
+    for(int i = 0; i < str.length(); i++){
+        System.out.print(str.charAt(i));
+    }
+    System.out.println("");
+}
---
+++ lesson 3/string.java
@@ -0,0 +1,5 @@
+package homework.controlflow;
+
+public class string {
+
+}
